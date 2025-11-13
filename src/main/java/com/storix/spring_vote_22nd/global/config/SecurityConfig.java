fa0c0ef74d6731364f478fed53a7456ae875906b
Immmii <leesa7118@gmail.com>
2025-11-13 23:02:33 +0900

    Chore: SecurityConfig JWT 설정 추가

--- src/main/java/com/storix/spring_vote_22nd/global/config/SecurityConfig.java
+++ src/main/java/com/storix/spring_vote_22nd/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.storix.spring_vote_22nd.global.config;
 
+import com.storix.spring_vote_22nd.global.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,6 +24,8 @@ import java.util.List;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)  throws Exception {
@@ -39,9 +43,10 @@ public class SecurityConfig {
                                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                                 // TODO: endpoint 결정 시 기본 api 추가 (일단 전부 허용)
                                 .anyRequest().permitAll()
-                );
+                )
 
-        // JWT 설정
+                // JWT 설정
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
