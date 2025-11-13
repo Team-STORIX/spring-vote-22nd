7298534b0712cc7ffa58bbc0db54a36aa300adcd
caminobelllo <alstpgml0812@gmail.com>
2025-11-13 12:26:59 +0900

    Chore: Spring Boot initial

---
+++ src/main/java/com/storix/spring_vote_22nd/SpringVote22ndApplication.java
@@ -0,0 +1,13 @@
+package com.storix.spring_vote_22nd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVote22ndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVote22ndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storix/spring_vote_22nd/SpringVote22ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storix.spring_vote_22nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVote22ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
