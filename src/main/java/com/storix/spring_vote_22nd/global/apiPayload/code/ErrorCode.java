57f890e74ee852fd0ba286f76892a1e7f76a5c0c
Immmii <leesa7118@gmail.com>
2025-11-18 18:25:25 +0900

    Chore: ErrorCode 추가

--- src/main/java/com/storix/spring_vote_22nd/global/apiPayload/code/ErrorCode.java
+++ src/main/java/com/storix/spring_vote_22nd/global/apiPayload/code/ErrorCode.java
@@ -13,7 +13,15 @@ public enum ErrorCode {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON_ERROR_002", "인증이 필요합니다"),
     FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON_ERROR_003", "접근이 금지되었습니다"),
     NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON_ERROR_004", "요청한 자원을 찾을 수 없습니다"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_ERROR_005", "서버 내부 오류가 발생했습니다");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_ERROR_005", "서버 내부 오류가 발생했습니다"),
+
+    // Token error
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_ERROR_001", "잘못된 토큰입니다"),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH_401_001", "토큰이 만료되었습니다. 토큰을 재 발급 해주세요"),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.FORBIDDEN, "AUTH_403_001", "토큰이 만료되었습니다. 재로그인 해주세요"),
+
+    // Login error
+    FAILED_LOGIN(HttpStatus.UNAUTHORIZED, "AUTH_ERROR_002", "회원 아이디와 비밀번호가 일치하지 않습니다. 다시 로그인 해주세요.");
 
     private final HttpStatus httpStatus;
     private final String code;
