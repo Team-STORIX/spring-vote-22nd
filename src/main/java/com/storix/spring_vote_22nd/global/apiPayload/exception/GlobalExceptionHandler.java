3e4cc9037ca77d062b7df80ae66042fd8551d0ba
Immmii <leesa7118@gmail.com>
2025-11-20 20:02:28 +0900

    Chore: GlobalExceptionHandler 로깅 추가

--- src/main/java/com/storix/spring_vote_22nd/global/apiPayload/exception/GlobalExceptionHandler.java
+++ src/main/java/com/storix/spring_vote_22nd/global/apiPayload/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.storix.spring_vote_22nd.global.apiPayload.exception;
 
 import com.storix.spring_vote_22nd.global.apiPayload.code.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,10 +11,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        log.error("[UNHANDLED_EXCEPTION] 발생", ex);
+
+        ErrorResponse response = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+
     @ExceptionHandler(STORIXCodeException.class)
     public ResponseEntity<ErrorResponse> STORIXCodeExceptionHandler (STORIXCodeException ex) {
 
         ErrorCode errorCode = ex.getErrorCode();
+        log.warn("[STORIX_CODE_EXCEPTION] code={}, message={}",
+                errorCode.getCode(), errorCode.getMessage(), ex);
         ErrorResponse response = new ErrorResponse(errorCode);
 
         return ResponseEntity
